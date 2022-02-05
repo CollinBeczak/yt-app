dda948f46a8dc15d8c91bdd34ae2d1ea3bf273f5
Collin Beczak <collinbeczak@Collins-MBP.home>
2022-02-05 16:30:24 -0600

    Test Commit

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,6 +1,6 @@
 const VideoDetail = ({ video }) => {
     if(!video) {
-        return <div>Loading...</div>;
+        return <div>Loading.....</div>;
     }
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
